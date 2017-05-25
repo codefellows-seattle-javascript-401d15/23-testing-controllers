0eebd22b69d687f7d7aee464a5b5e359580f1bd0
Benjamin <ayzenbergb@gmail.com>
2017-05-25 11:38:17 -0700

    some tests

--- lab-ben/test/cowsay-interface-test.js
+++ lab-ben/test/cowsay-interface-test.js
@@ -3,6 +3,8 @@
 const angular = require('angular');
 require('angular-mocks');
 
+const cowsay = require('cowsay-browser');
+
 describe('cowsay interface', function() {
   beforeEach(() => {
     angular.mock.module('cowsayApp');
@@ -14,7 +16,28 @@ describe('cowsay interface', function() {
   });
   afterEach(() => this.$rootScope.$apply());
 
-  it('should pass', () => {
-    expect(true).toEqual(true);
+  it('have 46 "cow" options', () => {
+    expect(this.cowsayCtrl.cowfiles.length).toEqual(46);
   });
+
+  it('should have default options', () => {
+    expect(this.cowsayCtrl.animal).toEqual('vader-koala');
+  });
+
+  it('should add an item to spoken on speak', () => {
+    this.cowsayCtrl.speak('Moo');
+    expect(this.cowsayCtrl.spoken).toEqual(cowsay.say({text: 'Moo', f: this.cowsayCtrl.animal}));
+  });
+
+  it('should add an item to the history on speak', () => {
+    this.cowsayCtrl.speak('moo?');
+    expect(this.cowsayCtrl.history[0]).toEqual(cowsay.say({text: 'moo?', f: this.cowsayCtrl.animal}));
+  });
+
+  it('should remove an item from the history on undo', () => {
+    this.cowsayCtrl.speak('moo?');
+    this.cowsayCtrl.undo();
+    expect(this.cowsayCtrl.history[0]).toEqual(undefined);
+  });
+
 });
