bb8a52ee254302e2da4dc767ab4392897f893955
David Teddy <davidteddy@outlook.com>
2017-05-25 15:12:43 -0700

    added more tests and they work

--- lab-teddy/webpack.config.js
+++ lab-teddy/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_module/,
+        exclude: /node_modules/,
         loader: 'babel-loader',
       },
       {
