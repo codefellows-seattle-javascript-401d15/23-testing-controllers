4ae64477f9a8f2e60153dcdd1f9b5867ef92efd4
michaeltreat <michael.leon.treat@gmail.com>
2017-05-31 18:26:12 -0700

    finished the test file, the entry.js and the index.html files

--- lab-miketreat/test/cowsay-controll-test.js
+++ lab-miketreat/test/cowsay-controll-test.js
@@ -1,1 +1,26 @@
 'use strict';
+
+require('angular');
+require('angular-mocks');
+
+describe('testing the CowsayController', function(){
+  beforeEach(()=>{
+    angular.mock.module('cowsayApp');
+    angular.mock.inject(($rootScope, $controller) =>{
+      this.$rootScope = $rootScope;
+      this.cowsayCtrl = new $controller('CowsayController');
+      this.cowsayCtrl.$onInit();
+    });
+  });
+  afterEach(()=> this.$rootScope.$apply());
+
+  it('Should be equal to true', () => {
+    expect(true).toEqual(true);
+  });
+
+  it('Have the title equal to "THERE IS NO COW LEVEL MAAAN"', () => {
+    console.log(this);
+    console.log(this.title);
+    expect(this.title).toEqual('THERE IS NO COW LEVEL MAAAN');
+  });
+});
